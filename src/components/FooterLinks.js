85b955fc52b2792408ed8c5163e710dff4aa2013
TaLoveous Goodwin <TaLoveous@expansivedesigns.com>
2022-02-27 08:31:09 -0500

    Updated footer links

--- src/components/FooterLinks.js
+++ src/components/FooterLinks.js
@@ -1,15 +1,14 @@
-import { BrowserRouter as Router } from "react-router-dom"
- 
+//import { BrowserRouter as Router } from "react-router-dom"
+
+
 export default function FooterLinks({ link1, link2, link3 }){
     return (
         <div className="footer_links">
-            <Router>
-                <div className='footer'>
-                    <a href={link1} className='footer_link' target="_blank" rel="noreferrer noopener">Facebook</a>
-                    <a href={link2} className='footer_link' target="_blank" rel="noreferrer noopener">Instagram</a>
-                    <a href={link3} className='footer_link' target="_blank" rel="noreferrer noopener">Twitter</a>
-                </div>
-            </Router>
+            <div className='footer'>
+                <a href={link1} className='footer_link' target="_blank" rel="noreferrer noopener">Facebook</a>
+                <a href={link2} className='footer_link' target="_blank" rel="noreferrer noopener">Instagram</a>
+                <a href={link3} className='footer_link' target="_blank" rel="noreferrer noopener">Twitter</a>
+            </div>
         </div>        
     )
 }
